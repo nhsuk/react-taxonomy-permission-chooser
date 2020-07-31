c901fab4810235e528d55e462d764826c6f561c4
Yohan Lebret <yohan.lebret@gmail.com>
2020-07-31 13:54:23 +0100

    Add dummy data for local development

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 
 import TaxonomyPermissionPanel from './components/TaxonomyPermissionPanel';
 
+import taxonomySample from './taxonomySample';
 
 ReactDOM.render(
   <TaxonomyPermissionPanel
+    actions={taxonomySample.actions}
+    vocabularyGroups={taxonomySample.vocabularyGroups}
   />,
   document.getElementById('root'),
 );
